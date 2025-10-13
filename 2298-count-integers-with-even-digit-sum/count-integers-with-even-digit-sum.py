b2ab9ba105076632ef154c08133e78cfb004e897
Ballem Nikitha <bnikitha9618@gmail.com>
2025-10-13 13:18:43 +0530

    Time: 27 ms (47.78%) | Memory: 12.4 MB (76.11%) - LeetSync

--- 2298-count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.py
+++ 2298-count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.py
@@ -3,10 +3,22 @@ class Solution(object):
         """
         :type num: int
         :rtype: int
-        """
+    
         c=0
         for i in range(1,num+1):
             digit_sum=sum(int(j) for j in str(i))
             if digit_sum%2==0:
                 c+=1
+        return c
+        """
+        c=0
+        for i in range(1,num+1):
+            if i<=9 and i%2==0:
+                c+=1
+            if i >9:
+                digit_sum=0
+                for j in str(i):
+                    digit_sum+=int(j)
+                if digit_sum%2==0:
+                    c+=1
         return c
